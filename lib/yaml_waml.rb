ab97c9464186f6fe0c93adafd103f50622ab0246
shintaro.kakutani <shintaro.kakutani@71ca0b86-4226-0410-a0b0-470639928cd4>
2007-01-05 03:29:11 +0000

    YAML WorkAround for Multibyte Languate plugin.
    this plugin originated by the blog entry written by id:Rommy(*).
    
    (*) http://d.hatena.ne.jp/Rommy/20061229/1167406811
    
    
    git-svn-id: https://yaml-waml.googlecode.com/svn/trunk/yaml_waml/trunk@12 71ca0b86-4226-0410-a0b0-470639928cd4

---
+++ lib/yaml_waml.rb
@@ -0,0 +1,43 @@
+require "yaml"
+
+class String
+  def is_binary_data?
+    false
+  end
+end
+
+yaml_waml = Module.new
+class << yaml_waml
+  def run
+    workaround_to_yaml_with_decode
+  end
+
+  private
+  def workaround_to_yaml_with_decode
+    ObjectSpace.each_object(Class){|klass|
+      klass.class_eval{
+        if method_defined?(:to_yaml) && !method_defined?(:to_yaml_with_decode)
+          def to_yaml_with_decode(*args)
+            result = to_yaml_without_decode(*args)
+            if result.kind_of? String
+              __decode_multibyte_string(result)
+            else
+              result
+            end
+          end
+          alias_method :to_yaml_without_decode, :to_yaml
+          alias_method :to_yaml, :to_yaml_with_decode
+          
+          def __decode_multibyte_string(string)
+            string.gsub(/\\x(\w{2})/){
+              [Regexp.last_match.captures.first.to_i(16)].pack("C")}
+          end
+          private :__decode_multibyte_string
+        end
+      }
+    }
+  end
+  
+end
+
+yaml_waml.run
---
+++ tasks/yaml_waml_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :yaml_waml do
+#   # Task goes here
+# end

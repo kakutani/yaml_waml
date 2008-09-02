7b8cd6cea0c9cab1cb00a898fa417ba5451828b4
Nobuhiro IMAI <nov@yo.rim.or.jp>
2008-09-03 02:08:05 +0900

    use "should be_eql" instead of should_eql

--- spec/to_yaml_spec.rb
+++ spec/to_yaml_spec.rb
@@ -1,14 +1,15 @@
+# -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + "/spec_helper"
 
 context "Japanese text" do
   specify "String should output string" do
-    "あ".to_yaml.should_eql <<-EXPECTED
+    "あ".to_yaml.should be_eql <<-EXPECTED
 --- "あ"
     EXPECTED
   end
   
   specify "Array-ed string should output string" do
-    ['あ','い'].to_yaml.should_eql <<-EXPECTED
+    ['あ','い'].to_yaml.should be_eql <<-EXPECTED
 --- 
 - "あ"
 - "い"
@@ -16,7 +17,7 @@ context "Japanese text" do
   end
   
   specify "Hash-ed string should output string" do
-    {'日本語' => ['出力']}.to_yaml.should_eql <<-EXPECTED
+    {'日本語' => ['出力']}.to_yaml.should be_eql <<-EXPECTED
 --- 
 "日本語": 
 - "出力"
@@ -25,7 +26,7 @@ context "Japanese text" do
   
   specify "mixed Array should output string" do
     actual = [["あ", "い"], {"う" => ["え"]}, Struct.new(:name).new("お")]
-    actual.to_yaml.should_eql <<-EXPECTED
+    actual.to_yaml.should be_eql <<-EXPECTED
 --- 
 - - "あ"
   - "い"

c5df1a6d0bf1973605a0be170a921681d62f407e
KAKUTANI Shintaro <shintaro@kakutani.com>
2008-09-02 11:56:57 +0900

    make all green

--- spec/to_yaml_spec.rb
+++ spec/to_yaml_spec.rb
@@ -1,37 +1,38 @@
+# -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + "/spec_helper"
-
+BLANK = " "
 context "Japanese text" do
   specify "String should output string" do
-    "あ".to_yaml.should_eql <<-EXPECTED
+    "あ".to_yaml.should == <<-EXPECTED
 --- "あ"
     EXPECTED
   end
-  
+
   specify "Array-ed string should output string" do
-    ['あ','い'].to_yaml.should_eql <<-EXPECTED
---- 
+    ['あ','い'].to_yaml.should == <<-EXPECTED
+---#{BLANK}
 - "あ"
 - "い"
     EXPECTED
   end
-  
+
   specify "Hash-ed string should output string" do
-    {'日本語' => ['出力']}.to_yaml.should_eql <<-EXPECTED
---- 
-"日本語": 
+    {'日本語' => ['出力']}.to_yaml.should == <<-EXPECTED
+---#{BLANK}
+"日本語":#{BLANK}
 - "出力"
     EXPECTED
   end
-  
+
   specify "mixed Array should output string" do
     actual = [["あ", "い"], {"う" => ["え"]}, Struct.new(:name).new("お")]
-    actual.to_yaml.should_eql <<-EXPECTED
---- 
+    actual.to_yaml.should == <<-EXPECTED
+---#{BLANK}
 - - "あ"
   - "い"
-- "う": 
+- "う":#{BLANK}
   - "え"
-- !ruby/struct: 
+- !ruby/struct:#{BLANK}
   name: "お"
     EXPECTED
   end

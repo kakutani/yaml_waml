3386dfd83ae90c213094983259ac0ba004bf27c0
KAKUTANI Shintaro <shintaro@kakutani.com>
2008-09-03 11:57:52 +0900

    adopted RSpec modern DSL syntax.

--- spec/to_yaml_spec.rb
+++ spec/to_yaml_spec.rb
@@ -41,25 +41,23 @@ describe YAML, "#to_yaml" do
 end
 
 describe YAML, ".dump" do
-  describe "w/ Japanese text" do
-    it "should output yaml string" do
-      YAML.dump("あ").should == <<-EXPECTED
+  it "should output yaml string" do
+    YAML.dump("あ").should == <<-EXPECTED
 --- "あ"
-      EXPECTED
-    end
+    EXPECTED
   end
 
-  specify "YAML.dump should be StringIO-friendly" do
+  it "should be StringIO-friendly" do
     actual = [["あ", "い"], {"う" => ["え"]}, Struct.new(:name).new("お")]
     io = StringIO.new
     YAML.dump(actual, io)
     io.string.should be_eql <<-EXPECTED
---- 
+---#{BLANK}
 - - "あ"
   - "い"
-- "う": 
+- "う":#{BLANK}
   - "え"
-- !ruby/struct: 
+- !ruby/struct:#{BLANK}
   name: "お"
     EXPECTED
   end
